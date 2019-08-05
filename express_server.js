8a96af2073057ffcfaa409a49ee9d589e656ca71
Ricky <chhoukdean184@msn.com>
2019-08-05 17:53:12 +0000

    Make the user redirect when they create a new shortURL and have that shortURL redirect to the original website if clicked

--- express_server.js
+++ express_server.js
@@ -38,9 +38,10 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  urlDatabase[generateRandomString()] = req.body.longURL;
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
   console.log(urlDatabase);
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortURL}`); // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls.json", (req, res) => {
@@ -57,6 +58,11 @@ app.get("/urls/:shortURL", (req, res) => {
     longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
