62ba591c1717af1870cf6fc1d3472496cc121e06
Ricky <chhoukdean184@msn.com>
2019-08-08 17:19:40 +0000

    Applied unit testing to all the helper functions

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,10 @@
 const { assert } = require("chai");
 
-const { userIDfromEmail } = require("../helpers.js");
+const {
+  userIDfromEmail,
+  generateRandomString,
+  urlsForUser
+} = require("../helpers.js");
 
 const testUsers = {
   userRandomID: {
@@ -15,6 +19,33 @@ const testUsers = {
   }
 };
 
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+    urlDate: "test",
+    clicks: 0
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+    urlDate: "test",
+    clicks: 0
+  },
+  test1: {
+    longURL: "https://www.googlea.ca",
+    userID: "userRandomID",
+    urlDate: "test",
+    clicks: 0
+  },
+  test2: {
+    longURL: "https://www.googles.ca",
+    userID: "userRandomID",
+    urlDate: "test",
+    clicks: 0
+  }
+};
+
 describe("getUserByEmail", function() {
   it("should return a user with valid email", function() {
     const user = userIDfromEmail("user@example.com", testUsers);
@@ -24,5 +55,40 @@ describe("getUserByEmail", function() {
   it("should return undefined if given an email that is not in our database", function() {
     const user = userIDfromEmail("userdsadasd@example.com", testUsers);
     assert.isUndefined(user, "Not defined");
+  });
+});
+
+describe("urlsForUser", function() {
+  it("should return the correct urls for the given user and database", function() {
+    const urls = urlsForUser("userRandomID", urlDatabase);
+    const expectedOutput = {
+      test1: {
+        longURL: "https://www.googlea.ca",
+        userID: "userRandomID",
+        urlDate: "test",
+        clicks: 0
+      },
+      test2: {
+        longURL: "https://www.googles.ca",
+        userID: "userRandomID",
+        urlDate: "test",
+        clicks: 0
+      }
+    };
+    assert.deepEqual(urls, expectedOutput);
+  });
+  it("should return an empty object if the the user has no urls", function() {
+    const urls = urlsForUser("doesntexist", urlDatabase);
+    const expectedOutput = {};
+    assert.deepEqual(urls, expectedOutput);
+  });
+});
+
+describe("generateRandomString", function() {
+  it("should return a string with the length of 5", function() {
+    let str = generateRandomString(urlDatabase);
+    let length = str.length;
+    const expectedOutput = 6;
+    assert.equal(length, expectedOutput);
   });
 });
